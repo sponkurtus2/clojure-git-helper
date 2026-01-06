283539fa5dba725ddeeb8cbce952a1e48c259e8c
sponkurtus2 <sponkurtus3@gmail.com>
2026-01-05 20:49:48 -0600

    Initialize project

---
+++ test/git_initializer/core_test.clj
@@ -0,0 +1,7 @@
+(ns git-initializer.core-test
+  (:require [clojure.test :refer :all]
+            [git-initializer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
