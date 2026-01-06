a984c878ea8fcf08acfa84ae3b9d454200c15234
sponkurtus2 <sponkurtus3@gmail.com>
2026-01-06 14:31:13 -0600

    Function added

--- src/git_initializer/core.clj
+++ src/git_initializer/core.clj
@@ -1,7 +1,20 @@
 (ns git-initializer.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.shell :as sh]))
+
+(def add ["add" "."])
+(defn commit [commit-msg] ["commit" "-m" commit-msg])
+(def push ["push"])
+
+(defn execute-process [commit-msg]
+  (let [commands [add (commit commit-msg) push]]
+    (doseq [args commands]
+      (let [result (apply sh/sh "git" args)]
+        (println (:out result))
+        (println (:err result))))))
+; sh/sh "git" i)
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!" args))
+  (execute-process args))
+
